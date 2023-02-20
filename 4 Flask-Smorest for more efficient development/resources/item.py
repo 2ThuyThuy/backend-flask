f6a4a0ba0f67e388341c64e2f737a27b8e91d081
Thùy Nguyễn <thuynguyen2462k1@gmail.com>
2023-02-20 17:26:26 +0700

    add flask

--- 4 Flask-Smorest for more efficient development/resources/item.py
+++ 4 Flask-Smorest for more efficient development/resources/item.py
@@ -1,15 +1,14 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-
+from schemas import ItemSchema, ItemUpdateSchema
 from db import items
 
 blp = Blueprint("Items", "items", description="Operations on items")
 
-
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -23,47 +22,36 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bab request. Ensure 'price', and 'name' are included in the JSON payload."
-            )
-            try:
-                item = items[item_id]
-                item |= item_data
-                return item
-            except KeyError:
-                abort(400, message="Item not found.")
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
+        try:
+            item = items[item_id]
+            item |= item_data
+
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")
+
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
-    def post(self):
-        item_data = request.get_json()
-
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bab request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
-            )
-
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
                 and item_data["store_id"] == item["store_id"]
             ):
                 abort(400, message=f"Item already exists.")
+
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id}
         items[item_id] = item
+
         return item
-
-
--- 4 Flask-Smorest for more efficient development/resources/store.py
+++ 4 Flask-Smorest for more efficient development/resources/store.py
@@ -1,20 +1,20 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 
-blp = Blueprint("stores", __name__, description="Operations on stores")
-
-
-blp = Blueprint("stores", __name__, description="Operations on stores")
+blp = Blueprint("Stores", "stores", description="Operations on stores")
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
+            # You presumably would want to include the store's items here too
+            # More on that when we look at databases
             return stores[store_id]
         except KeyError:
             abort(404, message="Store not found.")
@@ -29,22 +29,19 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
-    def get(self):
-        return {"stores": list(stores.values())}
+    @blp.response(200, StoreSchema(many=True))
+    def get(cls):
+        return stores.values()
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bab request. Ensure 'name' is included in the JSON payload."
-            )
-
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(cls, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message=f"Store already exists.")
+
         store_id = uuid.uuid4().hex
         store = {**store_data, "id": store_id}
         stores[store_id] = store
 
-        return store
+        return
