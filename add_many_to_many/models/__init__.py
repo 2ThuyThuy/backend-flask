274f67214bfae9105ae34bebbf01860dafdb9485
Thùy Nguyễn <thuynguyen2462k1@gmail.com>
2023-03-01 09:36:21 +0700

    many to many

---
+++ add_many_to_many/models/__init__.py
@@ -0,0 +1,5 @@
+from add_many_to_many.models.item import ItemModel
+from add_many_to_many.models.tag import TagModel
+from add_many_to_many.models.store import StoreModel
+from add_many_to_many.models.item_tags import ItemsTags
+
---
+++ add_many_to_many/models/item_tags.py
@@ -0,0 +1,12 @@
+from add_many_to_many.db import db
+
+
+class ItemsTags(db.Model):
+    __tablename__ = "items_tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
+
+    store = db.relationship("StoreModel", back_populates="tags")
+    items = db.relationship("ItemModel", back_populates="tag", secondary="items_tags")
